bd509672aac5e7e5fe7ea8839f0e0b2da91485b3
Artemprogrmist1337 <110352581+Artemprogrmist1337@users.noreply.github.com>
2022-08-04 15:47:11 +0200

    add main seit

--- djangoman/hello/views.py
+++ djangoman/hello/views.py
@@ -1,9 +1,9 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-# Create your views here. path("pisa", views.pisa, name="pisa")
+# Create your views here.
 
 def index(request):
-    return HttpResponse("hello world")
+    return render(request, "templates/hello/index.html")
 
 def pisa(request):
     return HttpResponse("<h1>TUT TAKOGO NET POKA</h1>")
