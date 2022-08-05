fe950b0af25780523359098a09a158b786d73518
Artemprogrmist1337 <110352581+Artemprogrmist1337@users.noreply.github.com>
2022-08-05 17:45:46 +0200

    code of slonday

--- djangoman/slonday/views.py
+++ djangoman/slonday/views.py
@@ -1,5 +1,25 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+import datetime
 # Create your views here.
 def index(request):
-    return HttpResponse("NET")
+    now = datetime.datetime.now()
+    """s = ""
+
+    if now.day == 12 and now.month == 8:
+        s = "пора кормить слонов!"
+    else:
+        s = "Слоны сегодня не едят"
+    return render(request, "slonday/index.html", {
+
+        "slday": s
+    })"""
+    if now.day == 12 and now.month == 8:
+        f = True
+    else:
+        f = False
+    return render(request, "slonday/index.html", {
+
+        "slday": f
+    })
+
