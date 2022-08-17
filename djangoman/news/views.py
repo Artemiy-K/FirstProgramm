04a07fe851731c4b6757346ba15c431dff12c4e7
Artemprogrmist1337 <110352581+Artemprogrmist1337@users.noreply.github.com>
2022-08-17 20:54:45 +0200

    get news in views

--- djangoman/news/views.py
+++ djangoman/news/views.py
@@ -1,7 +1,11 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import News
 
 # Create your views here.
 
 def index(request):
+    news = News.objects.all()
+    res = '<h1>Cпиисок новостей</h1>'
+    
     return HttpResponse("hello world")
