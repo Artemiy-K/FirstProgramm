6ff7e09378b045fcc1c063eb397f36d7b9c39323
Artemprogrmist1337 <110352581+Artemprogrmist1337@users.noreply.github.com>
2022-08-18 09:12:11 +0200

    translate admin

--- djangoman/news/admin.py
+++ djangoman/news/admin.py
@@ -2,4 +2,7 @@ from django.contrib import admin
 from .models import News
 # Register your models here.
 
-admin.site.register(News)
+
+class NewsAdmin(admin.ModelAdmin):
+    list_display = ('id', 'title', 'created_at', 'updated_at', 'is_published')
+admin.site.register(News, NewsAdmin)
