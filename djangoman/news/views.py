3d46da4ffd959a99ff4444acffafea3f4a5e143c
Artemprogrmist1337 <110352581+Artemprogrmist1337@users.noreply.github.com>
2022-08-21 16:20:00 +0200

    create icons of category

--- djangoman/news/views.py
+++ djangoman/news/views.py
@@ -1,14 +1,16 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-from .models import News
+from .models import News, Category
 
 # Create your views here.
 
 def index(request):
     news = News.objects.all()
+    categories = Category.objects.all()
     return render(request, 'news/index.html', {
         'news':news,
-        'title': 'список новостей'
+        'title': 'список новостей',
+        'category': categories,
     })
 
     return render(request, template_name='news/index.html', context=context)
