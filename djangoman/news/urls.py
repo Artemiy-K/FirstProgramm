a6be8015db762d185176993ab782d34674c8871d
Artemprogrmist1337 <110352581+Artemprogrmist1337@users.noreply.github.com>
2022-08-22 16:59:52 +0200

    add name of link

--- djangoman/news/urls.py
+++ djangoman/news/urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("", views.index, name="index"),
-    path("category/<int:category_id>/", views.get_category, name="get_category"),
+    path("", views.index, name="home"),
+    path("category/<int:category_id>/", views.get_category, name="category"),
 ]
