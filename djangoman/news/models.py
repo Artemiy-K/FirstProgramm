7279ca30e9ebfc03013344ecb07d81350bcf1a2b
Artemprogrmist1337 <110352581+Artemprogrmist1337@users.noreply.github.com>
2022-09-01 17:38:09 +0200

    create list_categories

--- djangoman/news/models.py
+++ djangoman/news/models.py
@@ -2,7 +2,7 @@ from django.db import models
 
 
 class News(models.Model):
-    title = models.CharField(max_length=300, verbose_name='Наименование')
+    title = models.CharField(max_length=150, verbose_name='Наименование')
     content = models.TextField(blank=True, verbose_name='Контент')
     created_at = models.DateTimeField(auto_now_add=True, verbose_name='Дата публикации')
     updated_at = models.DateTimeField(auto_now=True, verbose_name='Обновленно')
